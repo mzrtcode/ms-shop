ab41b3982d50623df68f05b8bf7c0e5109075ada
Stiven M <stvnm33@gmail.com>
2026-05-14 23:15:04 -0500

    Crear contrato para el evento de crear una orden

---
+++ order-service/src/main/java/com/ecommerse/order_service/event/OrderPlacedEvent.java
@@ -0,0 +1,13 @@
+package com.ecommerse.order_service.event;
+
+import java.util.List;
+
+public record OrderPlacedEvent(String orderNumber,
+                               String email,
+                               List<OrderItemEvent> items
+                               ) {
+
+    public record OrderItemEvent(String sku,
+                                 String price,
+                                 Integer quantity) { }
+}
